2c3848f122b343111bd30775e84f0587c0024431
Hugo Dettner Källander <hugodk@kth.se>
2025-02-17 15:50:30 +0100

    Refactor w black

--- aria_agents/__main__.py
+++ aria_agents/__main__.py
@@ -5,8 +5,9 @@ import subprocess
 import os
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'minio.env')
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "minio.env")
 load_dotenv(dotenv_path)
+
 
 def start_server(args):
     if args.login_required:
@@ -30,26 +31,30 @@ def start_server(args):
         f"--endpoint-url={os.getenv('MINIO_SERVER_URL')}",
         f"--endpoint-url-public={os.getenv('MINIO_SERVER_URL')}",
         "--s3-admin-type=minio",
-        "--startup-functions=aria_agents.chatbot:register_chat_service"
+        "--startup-functions=aria_agents.chatbot:register_chat_service",
     ]
     subprocess.run(command)
 
+
 def connect_to_server(args):
     from aria_agents.chatbot import connect_server
+
     if args.login_required:
         os.environ["BIOIMAGEIO_LOGIN_REQUIRED"] = "true"
     else:
         os.environ["BIOIMAGEIO_LOGIN_REQUIRED"] = "false"
     server_url = args.server_url
-    
+
     loop = asyncio.get_event_loop()
     loop.create_task(connect_server(server_url))
     loop.run_forever()
 
 
 def main():
-    parser = argparse.ArgumentParser(description="BioImage.IO Chatbot utility commands.")
-    
+    parser = argparse.ArgumentParser(
+        description="BioImage.IO Chatbot utility commands."
+    )
+
     subparsers = parser.add_subparsers()
 
     # Start server command
@@ -59,19 +64,19 @@ def main():
     parser_start_server.add_argument("--public-base-url", type=str, default="")
     parser_start_server.add_argument("--login-required", action="store_true")
     parser_start_server.set_defaults(func=start_server)
-    
+
     # Connect server command
     parser_connect_server = subparsers.add_parser("connect-server")
     parser_connect_server.add_argument("--server-url", default="https://ai.imjoy.io")
     parser_connect_server.add_argument("--login-required", action="store_true")
     parser_connect_server.set_defaults(func=connect_to_server)
 
-    
     args = parser.parse_args()
-    if hasattr(args, 'func'):
+    if hasattr(args, "func"):
         args.func(args)
     else:
         parser.print_help()
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
