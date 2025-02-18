2c2e0d4c2f150f2925f0a11f62e745201f0b801c
Hugo Dettner Källander <hugodk@kth.se>
2025-02-18 01:28:59 +0100

    Refactor AriaArtifacts

--- aria_agents/artifact_manager.py
+++ aria_agents/artifact_manager.py
@@ -1,5 +1,6 @@
 import httpx
 
+
 class AriaArtifacts:
     def __init__(self, server=None, event_bus=None):
         self.server = server
@@ -17,37 +18,35 @@ class AriaArtifacts:
 
     async def put(self, value, name):
         assert self._svc, "Please call `setup()` before using artifact manager"
-        
+
         # Artifact has to be staged before we can put files
         try:
             await self._svc.edit(artifact_id=self._artifact_id, version="stage")
             put_url = await self._svc.put_file(
-                artifact_id=self._artifact_id,
-                file_path=name
+                artifact_id=self._artifact_id, file_path=name
             )
             async with httpx.AsyncClient() as client:
                 response = await client.put(put_url, data=value, timeout=500)
             response.raise_for_status()
         except Exception as e:
             raise RuntimeError(f"File upload failed: {e}") from e
-        
+
         await self._svc.commit(self._artifact_id)
-        
+
         self._event_bus.emit("store_put", name)
         return name
 
     async def get_url(self, name: str):
         assert self._svc, "Please call `setup()` before using artifact manager"
         get_url = await self._svc.get_file(
-            artifact_id=self._artifact_id,
-            file_path=name
+            artifact_id=self._artifact_id, file_path=name
         )
         return get_url
 
     async def get(self, name: str):
         assert self._svc, "Please call `setup()` before using artifact manager"
         get_url = await self.get_url(name)
-        
+
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.get(get_url, timeout=500)
@@ -55,9 +54,9 @@ class AriaArtifacts:
         except Exception as e:
             print(f"File download failed: {e}")
             raise RuntimeError(f"File download failed: {e}") from e
-        
+
         return response.text
-    
+
     async def get_attachments(self):
         assert self._svc, "Please call `setup()` before using artifact manager"
         try:
@@ -65,15 +64,17 @@ class AriaArtifacts:
             return artifact_info.manifest.get("attachments", [])
         except Exception as e:
             print(f"Failed to get attachments: {e}")
-        
+
     async def get_attachment(self, name: str):
         assert self._svc, "Please call `setup()` before using artifact manager"
         attachments = await self.get_attachments()
-        reversed_attachments = attachments[::-1] # Reverse order to get the latest attachment first
+        reversed_attachments = attachments[
+            ::-1
+        ]  # Reverse order to get the latest attachment first
         for attachment in reversed_attachments:
             if attachment["name"] == name:
                 return attachment
         return None
-    
+
     def get_event_bus(self):
         return self._event_bus
